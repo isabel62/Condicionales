0ddf1e38746c28c0ecc27e0acbfcd540863fbefb
isabel62 <isabeliribarreng@hotmail.com>
2017-11-03 19:15:17 -0300

    calificaciones2

---
+++ src/condicionales/Calificaciones2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicionales;
+
+import Calificacion;
+
+ public static void main (String[]args);
+
+/**
+ *
+ * @author LAB08
+ */
+public class Calificaciones2 
+{
+
+  
+  float nota1,nota2,nota;
+ Cclificacion=intergerparse float(JOptionPane.showInputDialog("Ingrese nota1"));
+Calificacion= intergerparse.float(JOptionPane.showInputDialog("Ingrese nota2"));          
+Calificacion= intergerpars.float(JOptionPane.showInputDialog("Ingrese nota3));        
+    
+}
+if nota1,nota2,nota;
+Calificacion  =intergerparse
+
+{       
