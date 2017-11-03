e372f8774e16f5bb788893f030bf0dc98fdbd019
isabel62 <isabeliribarreng@hotmail.com>
2017-11-02 21:38:59 -0300

    promedio de calificaciones

---
+++ src/Calificacion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author LAB08
+ */
+public class Calificacion
+{
+
+public static void main(String[] args)
+{      
+float nota1;
+float nota2;
+float nota3;
+ nota1=Integer.parseInt(JOptionPane.showInputDialog("digite las notas1"));
+
+float=Integer.parseInt.(JPptionPane.showImputDialog("Ingrese notas2"));
+float=Integer.parseInt.(JOptionPane.showImputDialog("ingrese notas3"));
+float=Integer.parseInt.(JOptionpane.showImputDialog("Mostrar suma , dividido * 3"));
+    float  
+
+        }  
+}
